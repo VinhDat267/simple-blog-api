06686a9c6f70d4bc97dec0d06dbca3fd20724d9d
VinhDat <datvinh548@gmail.com>
2025-10-11 11:49:40 +0700

    feat: Add user retrieval and deletion endpoints; validate user creation request

--- src/main/java/com/example/blogapi/controller/UserController.java
+++ src/main/java/com/example/blogapi/controller/UserController.java
@@ -5,15 +5,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.blogapi.dto.UserCreateRequest;
 import com.example.blogapi.dto.UserResponse;
-import com.example.blogapi.entity.User;
 import com.example.blogapi.service.UserService;
 
+import io.micrometer.core.ipc.http.HttpSender.Response;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+
+import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -22,9 +28,25 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<UserResponse> createUser(@RequestBody UserCreateRequest request) {
+    public ResponseEntity<UserResponse> createUser(@Valid @RequestBody UserCreateRequest request) {
         UserResponse newUser = userService.createUser(request);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponse> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponse>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<UserResponse> deleteUser(@PathVariable Long id) {
+        userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
